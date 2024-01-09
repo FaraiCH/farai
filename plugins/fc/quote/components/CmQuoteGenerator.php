c66e8c009fef72468fd0d883b7229c4f20ab9570
FaraiCH <faraichaka@gmail.com>
2024-01-09 20:32:42 +0200

    Update

--- plugins/fc/quote/components/CmQuoteGenerator.php
+++ plugins/fc/quote/components/CmQuoteGenerator.php
@@ -14,6 +14,7 @@ use Renatio\DynamicPDF\Classes\PDF;
 class CmQuoteGenerator extends ComponentBase
 {
     public $user;
+    public $quote;
     public function componentDetails()
     {
         return [
@@ -27,12 +28,25 @@ class CmQuoteGenerator extends ComponentBase
      */
     public function defineProperties()
     {
-        return [];
+        return [
+            'item' => [
+                'title'       => 'Quote Item',
+                'description' => 'Slug for business item',
+                'default'     => '{{ :item }}',
+                'type'        => 'integer'
+            ],
+        ];
     }
 
     public function init()
     {
-        $this->user = Auth::getUser();
+        $item = $this->property('item');
+        if(!empty(Auth::getUser())){
+            $this->user = Auth::getUser();
+            if(!empty($item)){
+                $this->quote = QuoteGenerator::where('id', $item)->where('user_id',  $this->user->id)->first();;
+            }
+        }
     }
 
     public function onGenerate()
@@ -60,6 +74,7 @@ class CmQuoteGenerator extends ComponentBase
                 $quoteItem->price = $price[$count];
                 $quoteItem->quote_id = $quoteGenerator->id;
                 $quoteItem->save();
+                $count++;
             }
         }
         return \Redirect::to('/quote/preview/'. $quoteGenerator->id);
@@ -72,13 +87,13 @@ class CmQuoteGenerator extends ComponentBase
             return[
                 "#item_new_". $no => $this->renderPartial('@main_item', ['no' => $no]),
                 "#button" => $this->renderPartial('@add_button', ['no' => $no]),
-                \Flash::success("Item Created")
+                \Flash::success("Item Created For Edit!")
             ];
         }else{
             return[
                 "#item" => $this->renderPartial('@main_item', ['no' => 1]),
                 "#button"=> $this->renderPartial('@add_button', ['no' => 1]),
-                \Flash::success("Item Created")
+                \Flash::success("Item Created For Edit!")
             ];
         }
 
@@ -89,8 +104,43 @@ class CmQuoteGenerator extends ComponentBase
             $no = \Input::get('no');
             return[
                 "#item_". $no => $this->renderPartial('@empty', ['no' => $no]),
-                \Flash::success("Item Created")
+                \Flash::success("Item Deleted!")
             ];
         }
+    }
+
+    public function onEdit(){
+        if(\Input::has('id')){
+            $quoteGenerator = QuoteGenerator::find(\Input::get('id'));
+            $quoteGenerator->company = \Input::get("company");
+            $quoteGenerator->ponumber = \Input::get("ponumber");
+            if(!empty(\Input::file("logo")))
+                $quoteGenerator->logo = \Input::file("logo");
+            $quoteGenerator->address = \Input::get("from");
+            $quoteGenerator->billed = \Input::get("billed");
+            $quoteGenerator->shipped = \Input::get("shipped");
+            $quoteGenerator->prefix = \Input::get("prefix");
+            $quoteGenerator->terms = \Input::get("terms");
+            $quoteGenerator->user_id = Auth::getUser()->id;
+            $quoteGenerator->save();
+            $qtys = \Input::get('qty');
+            $descriptions = \Input::get('description');
+            $price = \Input::get('price');
+            $count = 0;
+            QuoteItem::where('quote_id', $quoteGenerator->id)->delete();
+            if(!empty($qtys)){
+                foreach ($qtys as $qty){
+                    $quoteItem = new QuoteItem();
+                    $quoteItem->units = $qty;
+                    $quoteItem->description = $descriptions[$count];
+                    $quoteItem->price = $price[$count];
+                    $quoteItem->quote_id = $quoteGenerator->id;
+                    $quoteItem->save();
+                    $count++;
+                }
+            }
+            return \Redirect::to('/quote/preview/'. $quoteGenerator->id);
+        }
+
     }
 }
